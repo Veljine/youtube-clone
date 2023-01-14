1cffe266db81bdb7c036426cb7ec08be4d2ccbad
Veljine <107668357+Veljine@users.noreply.github.com>
2023-01-13 20:15:21 -0800

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = ({selectedCategory, setSelectedCategory}) => {
     }}>
 
         {categories.map( category => (
-            <button className='category-btn' style={{ background: category.name ===  selectedCategory && '#FC1503'}} key={category.name} onClick={ () => setSelectedCategory(category.name)}>
+            <button className='category-btn' style={{ background: category.name ===  selectedCategory && '#FC1503', color: '#fff'}} key={category.name} onClick={ () => setSelectedCategory(category.name)}>
                 <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{category.icon}</span>
                 <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
             </button>
